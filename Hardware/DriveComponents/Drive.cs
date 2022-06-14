7820095c513af28213a4c00dce780a7d1df596da
9ualaBanana <9ualabanana@gmail.com>
2022-06-14 19:05:38 +0300

    Abstract storage drives.

---
+++ Hardware/DriveComponents/Drive.cs
@@ -0,0 +1,60 @@
+﻿namespace Hardware;
+
+public record Drive(
+    uint Id,
+    ulong Size,
+    ulong FreeSpace,
+    IEnumerable<DriveInfo> LogicalDrives)
+{
+    public static List<Drive> Info
+    {
+        get
+        {
+            if (OperatingSystem.IsWindows()) return WindowsDrive.Info;
+            //if (OperatingSystem.IsLinux()) return await LinuxGetForAll();
+            throw new PlatformNotSupportedException();
+        }
+    }
+
+    public static IEnumerable<string> LogicalDisksNamesFromDistinctDrives
+    {
+        get
+        {
+            if (OperatingSystem.IsWindows()) return WindowsDrive.LogicalDisksNamesFromDistinctDrives;
+            throw new PlatformNotSupportedException();
+        }
+    }
+
+    //async static Task<List<Disk>> LinuxGetForAll()
+    //{
+    //    return (await LinuxQueryDiskInfoForAll())
+    //        .Select(diskInfoQueryResult => GetDiskInfoFrom(diskInfoQueryResult))
+    //        .ToList();
+    //}
+
+    //async static Task<List<string>> LinuxQueryDiskInfoForAll()
+    //{
+    //    var startInfo = new ProcessStartInfo("sudo hwinfo")
+    //    {
+    //        CreateNoWindow = true,
+    //        RedirectStandardOutput = true,
+    //        Arguments = "--disk | egrep \"\\[|Unique ID|Capacity\""
+    //    };
+    //    return (await Process.Start(startInfo)!.StandardOutput.ReadToEndAsync())
+    //        .Split("[").ToList();
+    //}
+
+    //static Disk GetDiskInfoFrom(string linuxQueryResult)
+    //{
+    //    var linuxQueryResults = linuxQueryResult.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+    //    var diskSerialNumber = linuxQueryResults[1].Value();
+    //    string? caption = null;
+    //    string? fileSystem = null;
+
+    //    var size = double.Parse(linuxQueryResults[2].Value(true));
+    //    var storageSpace = new MemoryInfo(default, size);
+
+    //    return new(diskSerialNumber, caption, storageSpace, fileSystem);
+    //}
+}
---
+++ Hardware/DriveComponents/WindowsDrive.cs
@@ -0,0 +1,118 @@
+﻿using System.Management;
+using System.Runtime.Versioning;
+
+namespace Hardware;
+
+[SupportedOSPlatform("Windows")]
+internal static class WindowsDrive
+{
+    internal static List<Drive> Info
+    {
+        get
+        {
+            var logicalDisksNamesToDrivesMap = LogicalDisksNamesToDriveIdsMap;
+            var drivesIds = logicalDisksNamesToDrivesMap
+                .Select(its => its.DriveId)
+                .Distinct();
+
+            var drives = new List<Drive>();
+            foreach (var driveId in drivesIds)
+            {
+                drives.Add(
+                    new(
+                        driveId,
+                        GetTotalSpaceOnDrive(driveId),
+                        GetFreeSpaceOnDrive(driveId),
+                        GetLogicalDisksOnDrive(driveId))
+                    );
+            }
+            return drives;
+        }
+    }
+
+    internal static IEnumerable<string> LogicalDisksNamesFromDistinctDrives
+    {
+        get
+        {
+            return LogicalDisksNamesToDriveIdsMap
+                .DistinctBy(its => its.DriveId)
+                .Select(its => its.LogicalDiskName);
+        }
+    }
+
+    static ulong GetTotalSpaceOnDrive(uint driveId)
+    {
+        var logicalDiskOnDrive = GetLogicalDisksOnDrive(driveId);
+
+        ulong totalSpace = 0;
+        foreach (var logicalDisk in DriveInfo.GetDrives())
+        {
+            if (logicalDiskOnDrive.Any(its => its.Name == logicalDisk.Name))
+            {
+                totalSpace += (ulong)logicalDisk.TotalSize;
+            }
+        }
+        return totalSpace;
+    }
+
+    static ulong GetFreeSpaceOnDrive(uint driveId)
+    {
+        var logicalDisksOnDrive = GetLogicalDisksOnDrive(driveId);
+
+        ulong freeSpaceOnDrive = 0;
+        foreach (var logicalDisk in DriveInfo.GetDrives())
+        {
+            if (logicalDisksOnDrive.Any(its => its.Name == logicalDisk.Name))
+            {
+                freeSpaceOnDrive += (ulong)logicalDisk.AvailableFreeSpace;
+            }
+        }
+        return freeSpaceOnDrive;
+    }
+
+    static IEnumerable<DriveInfo> GetLogicalDisksOnDrive(uint driveId)
+    {
+        return LogicalDisksNamesToDriveIdsMap
+            .Where(its => its.DriveId == driveId)
+            .Select(its => new DriveInfo(its.LogicalDiskName));
+    }
+
+    static IList<(string LogicalDiskName, uint DriveId)>? _logicalDisksNamesToDriveIdsMap;
+    static IList<(string LogicalDiskName, uint DriveId)> LogicalDisksNamesToDriveIdsMap
+    {
+        get
+        {
+            if (_logicalDisksNamesToDriveIdsMap is not null) return _logicalDisksNamesToDriveIdsMap;
+
+            using var drivesToPartitionsSearcher = new ManagementObjectSearcher("SELECT Antecedent, Dependent FROM Win32_LogicalDiskToPartition");
+            using var drivesToPartitions = drivesToPartitionsSearcher.Get();
+
+            _logicalDisksNamesToDriveIdsMap = new List<(string, uint)>(drivesToPartitions.Count);
+            foreach (var driveToPartition in drivesToPartitions)
+            {
+                var logicalDiskName = ParseLogicalDiskName(driveToPartition);
+                var driveId = ParseDriveId(driveToPartition);
+
+                _logicalDisksNamesToDriveIdsMap.Add((logicalDiskName, driveId));
+            }
+
+            return _logicalDisksNamesToDriveIdsMap!;
+        }
+    }
+
+    static uint ParseDriveId(ManagementBaseObject driveToPartition)
+    {
+        return uint.Parse(
+            string.Join(
+                null,
+                driveToPartition["Antecedent"].ToString()!.Split("Disk #")[1].TakeWhile(c => char.IsDigit(c)))
+            );
+    }
+
+    static string ParseLogicalDiskName(ManagementBaseObject driveToPartition)
+    {
+        return string.Join(
+            null,
+            driveToPartition["Dependent"].ToString()!.SkipWhile(c => c != '"').Skip(1).Take(2));
+    }
+}
