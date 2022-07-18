04e02e4cef191cc7ab603325dd80f3bf390c5510
GIBSON <9ualabanana@gmail.com>
2022-07-18 12:13:25 +0300

    Escape + in bot messages.

--- ReepoBot/Services/Telegram/TelegramBot.cs
+++ ReepoBot/Services/Telegram/TelegramBot.cs
@@ -100,6 +100,7 @@ public static class TelegramHelperExtensions
             .Replace("]", @"\]")
             .Replace(".", @"\.")
             .Replace("-", @"\-")
+            .Replace("+", @"\+")
             .Replace("*", @"\*")
             .Replace("_", @"\_")
             .Replace(">", @"\>")
