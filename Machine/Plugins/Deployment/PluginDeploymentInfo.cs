879d424e423c331533cc566861e123144a656ccd
GIBSON <9ualabanana@gmail.com>
2022-07-25 22:44:46 +0300

    Separate concerns of deployment processes.

---
+++ Machine/Plugins/Deployment/PluginDeploymentInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Machine.Plugins.Installers;
+
+internal abstract record PluginDeploymentInfo(string? InstallationPath = default)
+{
+    internal string InstallerPath => Path.Combine(DownloadsDirectoryPath.Value, Path.GetFileName(DownloadUrl));
+    internal abstract string DownloadUrl { get; }
+    internal abstract ProcessStartInfo InstallationStartInfo { get; }
+
+
+    readonly static Lazy<string> DownloadsDirectoryPath = new(
+        () => SHGetKnownFolderPath(Guid.Parse("374DE290-123F-4565-9164-39C4925E467B"), default),
+        isThreadSafe: false);
+
+    [DllImport("shell32", CharSet = CharSet.Unicode, ExactSpelling = true, PreserveSig = false)]
+    static extern string SHGetKnownFolderPath(
+        [MarshalAs(UnmanagedType.LPStruct)] Guid rfid, uint dwFlags, nint hToken = default);
+}
---
+++ Machine/Plugins/Deployment/PluginsDeployer.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+
+namespace Machine.Plugins.Installers;
+
+internal class PluginsDeployer
+{
+    readonly HttpClient _httpClient;
+    readonly CancellationToken _cancellationToken;
+
+    internal PluginsDeployer(HttpClient httpClient, CancellationToken cancellationToken = default)
+    {
+        _httpClient = httpClient;
+        _cancellationToken = cancellationToken;
+    }
+
+    internal async Task DeployAsync(PluginDeploymentInfo deploymentInfo, bool deleteInstaller = true)
+    {
+        if (!File.Exists(deploymentInfo.InstallerPath))
+            await DownloadAsync(deploymentInfo);
+        await InstallAsync(deploymentInfo);
+
+        if (deleteInstaller) File.Delete(deploymentInfo.InstallerPath);
+    }
+
+    async Task DownloadAsync(PluginDeploymentInfo deploymentInfo)
+    {
+        var pluginInstaller = await _httpClient.GetStreamAsync(deploymentInfo.DownloadUrl, _cancellationToken);
+        using var fs = new FileStream(deploymentInfo.InstallerPath, FileMode.CreateNew, FileAccess.Write, FileShare.None, 4096, true);
+        await pluginInstaller.CopyToAsync(fs, _cancellationToken);
+    }
+
+    async Task InstallAsync(PluginDeploymentInfo deploymentInfo)
+    {
+        using var installation = Process.Start(deploymentInfo.InstallationStartInfo)!;
+        await installation.WaitForExitAsync(_cancellationToken);
+    }
+}
