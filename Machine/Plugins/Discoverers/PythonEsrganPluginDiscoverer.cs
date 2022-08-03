67426b73886991b8881930803021593c3a9d806d
GIBSON <9ualabanana@gmail.com>
2022-08-03 20:58:32 +0300

    Add discoverer for Python upscaler subplugin.

---
+++ Machine/Plugins/Discoverers/PythonEsrganPluginDiscoverer.cs
@@ -0,0 +1,12 @@
+﻿namespace Machine.Plugins.Discoverers;
+
+public class PythonEsrganPluginDiscoverer : PluginDiscoverer
+{
+    protected override IEnumerable<string> InstallationPathsImpl => new string[] { Path.Combine("plugins", "python") };
+
+    protected override string ParentDirectoryPattern => "ESRGAN";
+
+    protected override string ExecutableName => "test.py";
+
+    protected override Plugin GetDiscoveredPlugin(string executablePath) => new PythonEsrganPlugin(executablePath);
+}
