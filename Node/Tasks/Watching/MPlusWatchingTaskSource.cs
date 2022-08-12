bbc4bdb90763e0b43c8dd7bb802246108bfd4407
i3ym <deniso99@mail.ru>
2022-08-12 12:30:33 +0700

    Implement mplus watching task source

--- Node/Tasks/Watching/MPlusWatchingTaskSource.cs
+++ Node/Tasks/Watching/MPlusWatchingTaskSource.cs
@@ -1,17 +1,57 @@
+using Newtonsoft.Json;
+
 namespace Node.Tasks.Watching;
 
-// TODO:
 public class MPlusWatchingTaskSource : IWatchingTaskSource
 {
     public event Action<WatchingTaskFileAddedEventArgs>? FileAdded;
+    [JsonIgnore] readonly CancellationTokenSource TokenSource = new();
+
+    [MPlusDirectory] public readonly string Directory;
+    [DescriberIgnore] public string SinceIid { get; private set; } = string.Empty;
+
+    public MPlusWatchingTaskSource(string directory) => Directory = directory;
 
     public void StartListening(WatchingTask task)
     {
-        throw new NotImplementedException();
+        new Thread(async () =>
+        {
+            while (true)
+            {
+                if (TokenSource.IsCancellationRequested) return;
+
+                await start();
+                await Task.Delay(10_000);
+
+
+                async Task start()
+                {
+                    var res = await Api.ApiGet<ImmutableArray<NewItem>>($"{Api.TaskManagerEndpoint}/getmynewitems", "items", "Getting new items", ("sessionid", Settings.SessionId!), ("sinceiid", SinceIid), ("directory", Directory));
+                    res.LogIfError();
+                    if (!res) return;
+
+                    var items = res.Value;
+                    if (items.Length == 0) return;
+
+                    foreach (var item in items)
+                    {
+                        var input = new MPlusTaskInputInfo(item.Iid);
+                        FileAdded?.Invoke(new(item.Files.Jpeg.FileName, input));
+
+                        // placed here so if any of the items cause an exception, it would retry from the failed file
+                        SinceIid = item.Iid;
+                        NodeSettings.WatchingTasks.Save();
+                    }
+                }
+            }
+        })
+        { IsBackground = true }.Start();
     }
 
-    public void Dispose()
-    {
-        throw new NotImplementedException();
-    }
+    public void Dispose() => TokenSource.Cancel();
+
+
+    record NewItem(string Iid, NewItemFiles Files);
+    record NewItemFiles(NewItemFile Jpeg, NewItemFile Mov);
+    record NewItemFile(string FileName, long Size);
 }
