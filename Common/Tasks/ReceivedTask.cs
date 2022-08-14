ecfb74babf244e6c41fe3a0877ab1e3cf35d4aea
i3ym <deniso99@mail.ru>
2022-08-14 16:09:55 +0700

    Add task progress in tray icon

--- Common/Tasks/ReceivedTask.cs
+++ Common/Tasks/ReceivedTask.cs
@@ -2,6 +2,7 @@
 
 public record ReceivedTask(string Id, TaskInfo Info, bool ExecuteLocally) : ILoggable
 {
+    public double Progress = 0;
     string ILoggable.LogName => $"Task {Id}";
 
     public static string GenerateLocal() => "local_" + Guid.NewGuid();
--- NodeUI/TrayIndicator.cs
+++ NodeUI/TrayIndicator.cs
@@ -1,9 +1,61 @@
 using Avalonia.Controls.ApplicationLifetimes;
+using SkiaSharp;
 
 namespace NodeUI
 {
     public static class TrayIndicator
     {
+        readonly static Logger _logger = LogManager.GetCurrentClassLogger();
+        static readonly byte[] IconBytes, GreenIconBytes;
+        static readonly WindowIcon None, Red, Yellow, Green;
+
+        static TrayIndicator()
+        {
+            using (var iconstream = Resource.LoadStream(typeof(TrayIndicator).Assembly, "img.tray_icon.png"))
+                iconstream.Read(IconBytes = new byte[iconstream.Length]);
+
+            InitializeColoredIcons(out GreenIconBytes, out None, out Red, out Yellow, out Green);
+
+            {
+                var g = SKBitmap.Decode(Resource.LoadStream(typeof(TrayIndicator).Assembly, "img.tray_icon.png"));
+                using (var c = new SKCanvas(g))
+                    c.Clear();
+                GreenIconBytes = drawCircle(g, new SKColor(40, 255, 60, 255)).ToArray();
+            }
+
+
+            static void InitializeColoredIcons(out byte[] greenbytes, out WindowIcon none, out WindowIcon red, out WindowIcon yellow, out WindowIcon green)
+            {
+                var source = SKBitmap.Decode(IconBytes);
+
+                none = new WindowIcon(drawCircle(source, new SKColor(0, 0, 0, 0)));
+                red = new WindowIcon(drawCircle(source, new SKColor(255, 0, 0, 255)));
+                yellow = new WindowIcon(drawCircle(source, new SKColor(255, 255, 0, 255)));
+
+                var g = drawCircle(source, new SKColor(40, 255, 60, 255));
+                greenbytes = g.ToArray();
+                g.Seek(0, SeekOrigin.Begin);
+                green = new WindowIcon(g);
+            }
+            static MemoryStream drawCircle(SKBitmap source, SKColor color)
+            {
+                using var bitmap = source.Copy();
+                using (var canvas = new SKCanvas(bitmap))
+                {
+                    var size = bitmap.Width / 4;
+                    canvas.DrawCircle(bitmap.Width - 10 - size / 2, bitmap.Height - 10 - size / 2, size, new SKPaint() { Color = color });
+                    canvas.Flush();
+                }
+
+                var output = new MemoryStream();
+                bitmap.Encode(output, SKEncodedImageFormat.Png, 100);
+                output.Seek(0, SeekOrigin.Begin);
+
+                return output;
+            }
+
+        }
+
         public static void PreventClosing(this Window window)
         {
             window.Closing += (_, e) =>
@@ -22,27 +74,12 @@ namespace NodeUI
             }.ToImmutableArray();
 
             // TODO: remove four transparent pixels after fix
-            var icon = new TrayIcon() { ToolTipText = App.AppName, Icon = new WindowIcon(Resource.LoadStream(typeof(TrayIndicator).Assembly, "img.tray_icon.png")) };
+            var icon = new TrayIcon() { ToolTipText = App.AppName, Icon = None };
             icon.Clicked += (_, _) => open();
             icon.FixException();
 
-
-            NodeGlobalState.Instance.ExecutingTasks.SubscribeChanged(() => Dispatcher.UIThread.Post(() =>
-            {
-                var e = NodeGlobalState.Instance.ExecutingTasks;
-
-                if (e.Count == 0) icon.ToolTipText = LocalizedString.String("idle");
-                else
-                {
-                    var task = e[0];
-
-                    icon.ToolTipText = $@"
-                        {task.Id}
-                        {NodeGlobalState.Instance.GetPluginType(task)} {task.Info.TaskType}
-                        {task.Info.Input.ToString(Newtonsoft.Json.Formatting.None)}
-                        ".TrimLines();
-                }
-            }), true);
+            InitializeTooltipText(icon);
+            InitializeAnimatedIcon(icon);
 
             LocalizedString.ChangeLangWeakEvent.Subscribe(app, () => Dispatcher.UIThread.Post(updateMenus));
             updateMenus();
@@ -84,6 +121,88 @@ namespace NodeUI
                 FileList.KillProcesses();
                 Environment.Exit(0);
             }).Start();
+        }
+
+        static void InitializeTooltipText(TrayIcon icon)
+        {
+            NodeGlobalState.Instance.ExecutingTasks.SubscribeChanged(() => Dispatcher.UIThread.Post(() =>
+            {
+                var e = NodeGlobalState.Instance.ExecutingTasks;
+
+                if (e.Count == 0) icon.ToolTipText = LocalizedString.String("idle");
+                else
+                {
+                    var task = e[0];
+
+                    icon.ToolTipText = $@"
+                        {task.Id}
+                        {NodeGlobalState.Instance.GetPluginType(task)} {task.Info.TaskType}
+                        {task.Info.Input.ToString(Newtonsoft.Json.Formatting.None)}
+                        ".TrimLines();
+                }
+            }), true);
+        }
+        static void InitializeAnimatedIcon(TrayIcon icon)
+        {
+            var iconcache = new Dictionary<string, WindowIcon>();
+            int time = -1;
+
+            void update()
+            {
+                time++;
+
+                var wicon = getIcon();
+                if (icon.Icon != wicon)
+                    icon.Icon = wicon;
+
+
+                WindowIcon getIcon()
+                {
+                    if (NodeGlobalState.Instance.ExecutingTasks.Count == 0) return Yellow;
+                    // TODO: error
+
+                    if ((time / 3) % 2 == 0)
+                        return drawOnGreen("1/" + NodeGlobalState.Instance.ExecutingTasks.Count);
+
+                    return drawOnGreen((int) (NodeGlobalState.Instance.ExecutingTasks.FirstOrDefault()?.Progress ?? 0) + "%");
+                }
+                WindowIcon drawOnGreen(string text)
+                {
+                    if (iconcache.TryGetValue(text, out var cachedicon))
+                        return cachedicon;
+
+
+                    var bitmap = SKBitmap.Decode(GreenIconBytes);
+                    using (var canvas = new SKCanvas(bitmap))
+                    {
+                        var paint = new SKPaint(new SKFont(SKTypeface.Default, size: bitmap.Height * .8f * (MathF.Pow(.9f, text.Length)))) { Color = new SKColor(uint.MaxValue), TextAlign = SKTextAlign.Center, };
+                        canvas.DrawText(text, bitmap.Width / 2, bitmap.Height * .75f, paint);
+                    }
+
+                    var output = new MemoryStream();
+                    bitmap.Encode(output, SKEncodedImageFormat.Png, 100);
+                    output.Seek(0, SeekOrigin.Begin);
+
+                    return iconcache[text] = new WindowIcon(new Bitmap(output));
+                }
+            }
+
+
+            new Thread(() =>
+            {
+                while (true)
+                {
+                    Dispatcher.UIThread.Post(() =>
+                    {
+                        try { update(); }
+                        catch (Exception ex) { _logger.Error(ex); }
+                    });
+
+                    Thread.Sleep(1000);
+                }
+            })
+            { IsBackground = true }.Start();
+
         }
     }
 }
