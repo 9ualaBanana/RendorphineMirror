c3e9dbfe0b500a575196d9a23ca0e467ad635057
i3ym <deniso99@mail.ru>
2022-08-22 14:39:23 +0700

    Add software install method

--- Node/Registry/SoftwareRegistry.cs
+++ Node/Registry/SoftwareRegistry.cs
@@ -4,4 +4,6 @@ public static class SoftwareRegistry
 {
     public static ValueTask<OperationResult<ImmutableArray<SoftwareDefinition>>> GetSoftware() =>
         LocalApi.Send<ImmutableArray<SoftwareDefinition>>(Settings.RegistryUrl, "getsoft");
+
+    public static void Install(SoftwareVersionDefinition soft) => PowerShellInvoker.Invoke(soft.InstallScript);
 }
