d66f84dad7277e17bb9a98fcd1baf274f667b010
Alexandr Mikhailov <fermer721@mail.ru>
2022-08-24 15:31:21 +0700

    + Added test version of gallery in /gallery

--- Node/Listeners/PublicListener.cs
+++ Node/Listeners/PublicListener.cs
@@ -9,7 +9,7 @@ namespace Node.Listeners;
 
 public class PublicListener : ExecutableListenerBase
 {
-    protected override bool IsLocal => false;
+    protected override bool IsLocal => true;
 
     protected override async Task<HttpStatusCode> ExecuteGet(string path, HttpListenerContext context)
     {
@@ -126,6 +126,49 @@ public class PublicListener : ExecutableListenerBase
             writer.Write(info);
 
             return HttpStatusCode.OK;
+        }
+
+        if (path.StartsWith("gallery"))
+        {
+            string[] fileExt = { "jpg", "jpeg", "png" };
+            string taskDir = Init.TaskFilesDirectory;
+            
+
+            if (path == "gallery")
+            {
+                IEnumerable<string> foldersWithOutputs = Directory.GetDirectories(taskDir)
+                .Where(d => Directory.Exists(d + "/output") && Directory.EnumerateFiles(d + "/output").Any());
+                var tasksFiles = foldersWithOutputs
+                    .GroupBy(folder => fileExt.SelectMany(ext => Directory.EnumerateFiles(folder + "/output", "*." + ext)));
+
+                string info = "<html><body>";
+
+                foreach (var task in tasksFiles)
+                {
+                    info += $"<br><h3>{Path.GetFileName(task.First())}</h3>";
+                    foreach (string file in task.Key)
+                    {
+                        info += $"<img width='200px' src='{Path.GetFileName(task.First()) + "?name=" + Path.GetFileName(file)}'>";
+                    }
+                }
+
+                info += "</body></html>";
+
+                using var writer = new StreamWriter(response.OutputStream, leaveOpen: true);
+                writer.Write(info);
+                return HttpStatusCode.OK;
+            }
+            else
+            {
+                string name = context.Request.QueryString["name"] ?? "";
+                string file = taskDir + "/" + Path.GetFileName(Path.GetFileName(path)) + "/output/" + name;
+                if (File.Exists(file))
+                {
+                    using var writer = new BinaryWriter(response.OutputStream, System.Text.Encoding.Default, leaveOpen: true);
+                    writer.Write(File.ReadAllBytes(file));
+                    return HttpStatusCode.OK;
+                }
+            }
         }
 
         if (path == "")
