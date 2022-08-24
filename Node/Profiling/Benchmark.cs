8008da2402007b0bad4b31b8aae0b2adf5fef406
GIBSON <9ualabanana@gmail.com>
2022-08-24 15:47:10 +0300

    Remove stubs from node profiler benchmark.

--- Node/Profiling/Benchmark.cs
+++ Node/Profiling/Benchmark.cs
@@ -1,4 +1,6 @@
 ﻿using Benchmark;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Node.Profiling;
 
@@ -10,69 +12,53 @@ internal static class Benchmark
     readonly static string _sampleVideoPath = Path.Combine(_assetsPath, "4k_sample.mp4");
     readonly static FileBackedVersion LatestExecutedVersion = new(_assetsPath);
 
-    internal static bool ShouldBeRun => !isRun && IsUpdated;
-    static bool isRun;
-    /// <remarks>
-    /// Evaluated once upon node start.
-    /// </remarks>
-    static bool IsUpdated =>
-        !LatestExecutedVersion.Exists || LatestExecutedVersion < BenchmarkMetadata.Version;
     /// <summary>
     /// true if <see cref="Benchmark"/> wasn't run yet after it had been updated; false, otherwise.
     /// </summary>
+    internal static bool ShouldBeRun => !isCompleted && IsUpdated;
+    static bool isCompleted;
+    /// <remarks>
+    /// Evaluated once upon node start.
+    /// </remarks>
+    static bool IsUpdated => !LatestExecutedVersion.Exists || LatestExecutedVersion < BenchmarkMetadata.Version;
 
     internal static async Task<object> RunAsync(int testDataSize)
     {
-        if (Environment.GetCommandLineArgs().Contains("release"))
-        {
-            return new
-            {
-                cpu = new
-                {
-                    rating = 10000000,
-                    pratings = new { ffmpeg = 100 },
-                    load = 0.0001,
-                },
-                gpu = new
-                {
-                    rating = 10000000,
-                    pratings = new { ffmpeg = 100 },
-                    load = 0.0001,
-                },
-                ram = new { total = 32678000000, free = 16678000000, },
-                disks = new[] { new { freespace = 326780000000, writespeed = 32677000000 } },
-            };
-        }
+        var result = await RunAsyncCore(testDataSize);
 
+        isCompleted = true;
+        LatestExecutedVersion.Update(BenchmarkMetadata.Version);
+
+        _logger.Info("Benchmark completed:\n{BenchmarkResults}", JsonConvert.SerializeObject(result, Formatting.None));
+
+        return result;
+    }
+
+    static async Task<object> RunAsyncCore(int testDataSize)
+    {
         using var _ = new FuncDispose(NodeGlobalState.Instance.ExecutingBenchmarks.Clear);
         var benchs = NodeGlobalState.Instance.ExecutingBenchmarks;
         benchs.Execute(() => benchs["cpu"] = benchs["gpu"] = benchs["ram"] = benchs["disks"] = null);
 
-        var cpu = await ComputePayloadWithCPUBenchmarkResultsAsync(testDataSize);
-        benchs["cpu"] = Newtonsoft.Json.Linq.JToken.FromObject(cpu);
-        var gpu = await ComputePayloadWithGPUBenchmarkResultsAsync();
-        benchs["gpu"] = Newtonsoft.Json.Linq.JToken.FromObject(gpu);
-        var ram = GetRAMPayload();
-        benchs["ram"] = Newtonsoft.Json.Linq.JToken.FromObject(ram);
-        var disks = await ComputePayloadWithDrivesBenchmarkResultsAsync(testDataSize);
-        benchs["disks"] = Newtonsoft.Json.Linq.JToken.FromObject(disks);
+        var cpu = await GetCpuBenchmarkResultsAsObjectAsync(testDataSize);
+        benchs["cpu"] = JToken.FromObject(cpu);
+        var gpu = await GetGpuBenchmarkResultsAsObjectAsync();
+        benchs["gpu"] = JToken.FromObject(gpu);
+        var ram = GetRamAsObject();
+        benchs["ram"] = JToken.FromObject(ram);
+        var disks = await GetDrivesBenchmarkResultsAsObjectAsync(testDataSize);
+        benchs["disks"] = JToken.FromObject(disks);
 
-        isRun = true;
-        LatestExecutedVersion.Update(BenchmarkMetadata.Version);
-
-        var output = new
+        return new
         {
             cpu,
             gpu,
             ram,
             disks,
         };
-        _logger.Info("Benchmark completed: {BenchmarkResults}", Newtonsoft.Json.JsonConvert.SerializeObject(output, Newtonsoft.Json.Formatting.None));
-
-        return output;
     }
 
-    static async Task<object> ComputePayloadWithCPUBenchmarkResultsAsync(int testDataSize)
+    static async Task<object> GetCpuBenchmarkResultsAsObjectAsync(int testDataSize)
     {
         double ffmpegRating = default;
         try
@@ -84,35 +70,36 @@ internal static class Benchmark
         {
             rating = (await new ZipBenchmark(testDataSize).RunAsync()).Bps,
             pratings = new { ffmpeg = ffmpegRating },
-            load = .0001,
+            load = CPU.Info.First().LoadPercentage,
         };
     }
 
-    static async Task<object> ComputePayloadWithGPUBenchmarkResultsAsync()
+    static async Task<object> GetGpuBenchmarkResultsAsObjectAsync()
 
     {
         double ffmpegRating = default;
+        uint load = default;
         try
         {
             ffmpegRating = (await new FFmpegBenchmark(_sampleVideoPath, $"{Path.Combine(_assetsPath, "ffmpeg")}").RunOnGpuAsync()).Bps;
+            load = GPU.Info.First().LoadPercentage;
         }
         catch (Exception) { }
         return new
         {
-            rating = 10_000_000, // TODO: rating load etc
+            rating = ffmpegRating,
             pratings = new { ffmpeg = ffmpegRating },
-            load = .0001,
+            load,
         };
     }
 
-    static async Task<IEnumerable<object>> ComputePayloadWithDrivesBenchmarkResultsAsync(int testDataSize)
+    static async Task<IEnumerable<object>> GetDrivesBenchmarkResultsAsObjectAsync(int testDataSize)
     {
         var drivesBenchmarkResults = new List<(BenchmarkResult Read, BenchmarkResult Write)>();
         var readWriteBenchmark = new ReadWriteBenchmark(testDataSize);
+
         foreach (var logicalDiskName in Drive.LogicalDisksNamesFromDistinctDrives)
-        {
             drivesBenchmarkResults.Add(await readWriteBenchmark.RunAsync(logicalDiskName));
-        }
 
         return Drive.Info.Zip(drivesBenchmarkResults)
             .Select(zip => new
@@ -122,7 +109,7 @@ internal static class Benchmark
             });
     }
 
-    static object GetRAMPayload()
+    static object GetRamAsObject()
     {
         var ramInfo = RAM.Info;
         return new
