2670cd24723c31fe768eb296e07b431ab78639b6
i3ym <deniso99@mail.ru>
2022-09-06 16:48:26 +0700

    Remove autorelogin

--- Node/Program.cs
+++ Node/Program.cs
@@ -37,25 +37,7 @@ if (Settings.SessionId is not null)
 {
     logger.Info($"Session ID is present. Email: {Settings.Email}; User ID: {Settings.UserId}; {(Settings.IsSlave == true ? "slave" : "non-slave")}");
 
-    // TODO: remove auth check when sids stop expiring
-    logger.Info("Checking authentication...");
-
-    var check = await Apis.GetMyTasksAsync(TaskState.Queued, sessionId: Settings.SessionId);
-    if (check) logger.Info("Session ID is valid");
-    else
-    {
-        if (check.AsString().Contains("Invalid M+ session ID.", StringComparison.OrdinalIgnoreCase))
-        {
-            logger.Info("Session ID was invalid, relogging in a slave mode");
-
-            // node name should stay the same
-            var auth = await SessionManager.AutoAuthAsync(Settings.Email);
-            auth.LogIfError();
-        }
-        else check.LogIfError("Error checking authentication: {0}");
-    }
-
-    if (Settings.SessionId is not null && !Debugger.IsAttached)
+    if (!Debugger.IsAttached)
         Process.Start(new ProcessStartInfo(FileList.GetNodeUIExe(), "hidden"));
 }
 else
