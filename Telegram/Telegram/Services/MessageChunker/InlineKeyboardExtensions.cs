ea29134a0ffa4c0c0c88d7544ebcf309e20ad5e0
GIBSON <9ualabanana@gmail.com>
2022-09-25 15:41:44 +0300

    Add inline keyboard markup for chunked messages.

---
+++ Telegram/Telegram/Services/MessageChunker/InlineKeyboardExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Telegram.Bot.Types.ReplyMarkups;
+
+namespace Telegram.Telegram.Services.MessageChunker;
+
+public static class InlineKeyboardExtensions
+{
+    public static InlineKeyboardMarkup WithAddedButtonNext(this InlineKeyboardMarkup inlineKeyboardMarkup) => new(
+        inlineKeyboardMarkup.InlineKeyboard.Append(new InlineKeyboardButton[]
+        { InlineKeyboardButton.WithCallbackData(">", ChunkedMessageCallbackQueryFlags.Next.ToString()) }));
+
+    public static InlineKeyboardMarkup WithAddedButtonPrevious(this InlineKeyboardMarkup inlineKeyboardMarkup) => new(
+        inlineKeyboardMarkup.InlineKeyboard.Append(new InlineKeyboardButton[]
+        { InlineKeyboardButton.WithCallbackData("<", ChunkedMessageCallbackQueryFlags.Previous.ToString()) }));
+}
