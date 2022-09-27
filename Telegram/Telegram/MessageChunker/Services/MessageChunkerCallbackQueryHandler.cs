57b93c803414034aa6c014cf32b370c809d7dbec
GIBSON <9ualabanana@gmail.com>
2022-09-27 17:16:58 +0300

    Add message chunker callback query handler.

---
+++ Telegram/Telegram/MessageChunker/Services/MessageChunkerCallbackQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Telegram.MessageChunker.Models;
+using Telegram.Telegram.Updates;
+
+namespace Telegram.Telegram.MessageChunker.Services;
+
+public class MessageChunkerCallbackQueryHandler : TelegramCallbackQueryHandlerBase
+{
+    readonly ChunkedMessagesAutoStorage _chunkedMessagesAutoStorage;
+
+
+    public MessageChunkerCallbackQueryHandler(
+        ILogger<TelegramCallbackQueryHandler> logger,
+        TelegramBot bot,
+        ChunkedMessagesAutoStorage chunkedMessagesAutoStorage) : base(logger, bot)
+    {
+        _chunkedMessagesAutoStorage = chunkedMessagesAutoStorage;
+    }
+
+
+    public override async Task HandleAsync(Update update)
+    {
+        var messageChunkerCallbackData = new MessageChunkerCallbackData(CallbackDataFrom(update));
+        var messageToEdit = _chunkedMessagesAutoStorage[update.CallbackQuery!.Message!.MessageId];
+        if (messageToEdit is null) return;
+
+        var newMessageContent = messageChunkerCallbackData.Value.HasFlag(MessageChunkerCallbackQueryFlags.Previous) ? messageToEdit.ChunkedText.PreviousChunk() : messageToEdit.ChunkedText.NextChunk();
+        await Bot.EditMessageTextAsync(update.CallbackQuery.Message.Chat.Id, messageToEdit.Message.MessageId, newMessageContent, ParseMode.MarkdownV2);
+        await Bot.EditMessageReplyMarkupAsync(update.CallbackQuery.Message.Chat.Id, messageToEdit.Message.MessageId, TextChunker.ReplyMarkupFor(messageToEdit.ChunkedText));
+    }
+}
--- Telegram/Telegram/Updates/TelegramCallbackQueryHandler.cs
+++ Telegram/Telegram/Updates/TelegramCallbackQueryHandler.cs
@@ -1,5 +1,7 @@
 ﻿using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Telegram.MessageChunker.Models;
+using Telegram.Telegram.MessageChunker.Services;
 using Telegram.Telegram.Updates.Images.Models;
 using Telegram.Telegram.Updates.Images.Services;
 using Telegram.Telegram.Updates.Tasks.Models;
@@ -9,31 +11,35 @@ namespace Telegram.Telegram.Updates;
 
 public class TelegramCallbackQueryHandler : TelegramUpdateHandler
 {
+    readonly MessageChunkerCallbackQueryHandler _messageChunkerHandler;
     readonly ImageProcessingCallbackQueryHandler _imageProcessingHandler;
     readonly VideoProcessingCallbackQueryHandler _videoProcessingHandler;
     readonly TaskCallbackQueryHandler _taskHandler;
 
 
-
     public TelegramCallbackQueryHandler(
         ILogger<TelegramCallbackQueryHandler> logger,
         TelegramBot bot,
+        MessageChunkerCallbackQueryHandler messageChunkerHandler,
         ImageProcessingCallbackQueryHandler imageProcessingHandler,
         VideoProcessingCallbackQueryHandler videoProcessingHandler,
         TaskCallbackQueryHandler taskHandler) : base(logger, bot)
     {
+        _messageChunkerHandler = messageChunkerHandler;
         _imageProcessingHandler = imageProcessingHandler;
         _videoProcessingHandler = videoProcessingHandler;
         _taskHandler = taskHandler;
     }
 
 
-
+    // Refactor by enumearating over services of type TelegramCallbackData like in Commands.
     public override async Task HandleAsync(Update update)
     {
         var callbackQuery = update.CallbackQuery!;
         await AnswerCallbackQueryAsync(callbackQuery);
 
+        if (MessageChunkerCallbackData.Matches(callbackQuery.Data!))
+        { await _messageChunkerHandler.HandleAsync(update); return; }
         if (ImageProcessingCallbackData.Matches(callbackQuery.Data!))
         { await _imageProcessingHandler.HandleAsync(update); return; }
         if (VideoProcessingCallbackData.Matches(callbackQuery.Data!))
