ce80f38f9eb9e19cb5be9914b47440a5c14c9d66
GIBSON <9ualabanana@gmail.com>
2022-09-27 13:17:01 +0300

    Add chunked message.

---
+++ Telegram/Telegram/MessageChunker/Models/ChunkedMessage.cs
@@ -0,0 +1,11 @@
+﻿using Telegram.Bot.Types;
+
+namespace Telegram.Telegram.MessageChunker.Models;
+
+public record ChunkedMessage(Message Message, ChunkedText ChunkedText)
+{
+    public ChunkedMessage(Message telegramMessage)
+        : this(telegramMessage, new(telegramMessage.Text!))
+    {
+    }
+}
