0ff1179839efd352872278229ba32a0d2e10740e
GIBSON <9ualabanana@gmail.com>
2022-10-29 21:29:22 +0400

    Add `I3DModelUploader` interface.

---
+++ Transport/Upload/_3DModelsUpload/Services/I3DModelUploader.cs
@@ -0,0 +1,8 @@
+﻿using Transport.Upload._3DModelsUpload.Models;
+
+namespace Transport.Upload._3DModelsUpload.Services;
+
+internal interface I3DModelUploader<TMetadata> where TMetadata : _3DModelMetadata
+{
+    Task UploadAsync(Composite3DModel composite3DModel, TMetadata metadata);
+}
