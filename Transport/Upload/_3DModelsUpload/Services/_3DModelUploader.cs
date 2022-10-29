f9facdbf56fb045cecff88d3035dba23d4d5761e
GIBSON <9ualabanana@gmail.com>
2022-10-29 13:09:24 +0400

    Add `_3DModelUploader` static class.

---
+++ Transport/Upload/_3DModelsUpload/Services/_3DModelUploader.cs
@@ -0,0 +1,16 @@
+﻿using Transport.Upload._3DModelsUpload.Models;
+
+namespace Transport.Upload._3DModelsUpload.Services;
+
+public static class _3DModelUploader
+{
+    public async Task UploadAsync(_3DModel _3DModel) =>
+        await UploadAsync(new Composite3DModel(previews: null, _3DModel._ToModelParts()));
+
+    public async Task UploadAsync(Composite3DModel composite3DModel)
+    {
+        composite3DModel.Archive();
+
+    protected abstract Task UploadAsyncCore(Composite3DModel composite3DModel);
+    }
+}
