9feb595e4c4be992ac517688a6fbf872ca7995b4
GIBSON <9ualabanana@gmail.com>
2022-11-07 18:03:08 +0400

    Add CGTrader addresses.

---
+++ Transport/Upload/_3DModelsUpload/CGTrader/Models/CGTraderAddress.cs
@@ -0,0 +1,8 @@
+﻿namespace Transport.Upload._3DModelsUpload.CGTrader.Models;
+
+internal static class CGTraderAddress
+{
+    internal const string WWW = $"www.{Domain}";
+    internal const string Https = $"https://{WWW}";
+    internal const string Domain = "cgtrader.com";
+}
