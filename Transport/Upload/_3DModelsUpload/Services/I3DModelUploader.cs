8cf7e2dbbbe6f059b62aeb19adff8ad34c480178
GIBSON <9ualabanana@gmail.com>
2022-11-07 19:12:49 +0400

    Turn `_I3DModelUploader` interface to ABC.

--- Transport/Upload/_3DModelsUpload/Services/I3DModelUploader.cs
+++
@@ -1,8 +0,0 @@
-﻿using Transport.Upload._3DModelsUpload.Models;
-
-namespace Transport.Upload._3DModelsUpload.Services;
-
-internal interface I3DModelUploader<TMetadata> where TMetadata : _3DModelMetadata
-{
-    Task UploadAsync(Composite3DModel composite3DModel, TMetadata metadata);
-}
---
+++ Transport/Upload/_3DModelsUpload/Services/_3DModelUploaderBase.cs
@@ -0,0 +1,19 @@
+﻿using Transport.Upload._3DModelsUpload.CGTrader.Models;
+using Transport.Upload._3DModelsUpload.Models;
+
+namespace Transport.Upload._3DModelsUpload.Services;
+
+internal abstract class _3DModelUploaderBase<TMetadata> where TMetadata : _3DModelMetadata
+{
+    protected HttpClient HttpClient;
+
+
+    protected _3DModelUploaderBase(HttpClient httpClient) => HttpClient = httpClient;
+
+
+    internal abstract Task UploadAsync(
+        CGTraderNetworkCredential credential,
+        Composite3DModel composite3DModel,
+        TMetadata metadata,
+        CancellationToken cancellationToken = default);
+}
