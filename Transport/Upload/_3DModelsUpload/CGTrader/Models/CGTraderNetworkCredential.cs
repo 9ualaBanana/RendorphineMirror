7c17a0d92ad3071e1603350f8b049e1760374c16
GIBSON <9ualabanana@gmail.com>
2022-11-07 18:09:03 +0400

    Add `CGTraderNetworkCredential` class.

---
+++ Transport/Upload/_3DModelsUpload/CGTrader/Models/CGTraderNetworkCredential.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using System.Security;
+
+namespace Transport.Upload._3DModelsUpload.CGTrader.Models;
+
+public class CGTraderNetworkCredential : NetworkCredential
+{
+    public bool RememberMe;
+    internal JsonContent AsJson => _asJson ??= JsonContent.Create(
+        new
+        {
+            user = new
+            {
+                login = UserName,
+                password = Password,
+                remember_me = RememberMe.ToString().ToLower()
+            },
+            location = "/users/login"
+        });
+    JsonContent? _asJson;
+
+
+    public CGTraderNetworkCredential(string? userName, string? password, bool rememberMe)
+        : base(userName, password, CGTraderAddress.Domain)
+    {
+        RememberMe = rememberMe;
+    }
+
+    public CGTraderNetworkCredential(string? userName, SecureString password, bool rememberMe)
+        : base(userName, password, CGTraderAddress.Domain)
+    {
+        RememberMe = rememberMe;
+    }
+}
