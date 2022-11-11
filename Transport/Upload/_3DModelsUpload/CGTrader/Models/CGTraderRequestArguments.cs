2d3862e8fd26ccce3423732b2a511a9f96ad7bf3
GIBSON <9ualabanana@gmail.com>
2022-11-11 16:20:09 +0400

    Update `CaptchaRequestArguments`.

--- Transport/Upload/_3DModelsUpload/CGTrader/Models/CGTraderRequestArguments.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Transport.Upload._3DModelsUpload.CGTrader.Models;
-
-internal static class CGTraderRequestArguments
-{
-    internal const string rt = "1667826784473";
-    internal const string ss = "S1b7905206-4af0-4105-a299-09ab4e6874ba";
-}
---
+++ Transport/Upload/_3DModelsUpload/CGTrader/Models/CaptchaRequestArguments.cs
@@ -0,0 +1,18 @@
+﻿namespace Transport.Upload._3DModelsUpload.CGTrader.Models;
+
+internal static class CaptchaRequestArguments
+{
+    internal const string act = "$";
+    internal const string bd = CGTraderUri.WWW;
+    internal const string fa = "TgSlECMZdJiH8biyqbHkM25dZB5uOsyl7venyCoFqTkj1w9y3Tph8L794dMd";
+    internal const string kt = "a6HLIqylE6wiX_UC1xFbZ9jCJJfKV-1QyR9JslJSRyzWM1c3AEVYh9t2hqShuNSh";
+    internal const string lf = "1";
+    internal const string lg = "en";
+    internal const string qh = "$";
+    internal static string rt => DateTime.UtcNow.AsUnixTimestamp();
+    internal const string ss = "S1e09c4e49-9c13-44b5-942e-a9485fdae783";
+    internal const string st = "sqiz";
+    internal const string tl = "$";
+    internal const string tp = "s";
+    internal const string tsh = "TH[31e658709b71e41628a6dd03cbf73e1f]";
+}
