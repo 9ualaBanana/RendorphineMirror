2b0f97c328a98774003da30b6eb0ada54c9422b3
GIBSON <9ualabanana@gmail.com>
2022-11-11 07:43:26 +0400

    Add `CGTraderCaptcha`.

---
+++ Transport/Upload/_3DModelsUpload/CGTrader/Models/CGTraderCaptcha.cs
@@ -0,0 +1,28 @@
+﻿namespace Transport.Upload._3DModelsUpload.CGTrader.Models;
+
+public class CGTraderCaptcha
+{
+    readonly string _asBase64;
+
+    #region Initialization
+
+    internal static CGTraderCaptcha _FromBase64String(string? captcha) => new(captcha ?? string.Empty);
+
+    CGTraderCaptcha(string captchaAsBase64)
+    {
+        _asBase64 = captchaAsBase64;
+    }
+
+    #endregion
+
+    internal string _Solve()
+    {
+        return string.Empty;
+    }
+
+    #region Casts
+
+    public static implicit operator string(CGTraderCaptcha captcha) => captcha._asBase64;
+
+    #endregion
+}
