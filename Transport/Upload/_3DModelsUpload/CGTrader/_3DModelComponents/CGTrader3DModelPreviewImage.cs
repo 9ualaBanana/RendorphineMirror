2939927929358ea326cf83f4fd5b12c8979abdaa
GIBSON <9ualabanana@gmail.com>
2022-11-26 02:36:49 +0400

    Fix `CGTrader3DModelPreviewImage` record.

--- Transport/Upload/_3DModelsUpload/CGTrader/_3DModelComponents/CGTrader3DModelPreviewImage.cs
+++ Transport/Upload/_3DModelsUpload/CGTrader/_3DModelComponents/CGTrader3DModelPreviewImage.cs
@@ -1,72 +1,24 @@
-﻿using Common;
-using System.Net.Mime;
-using System.Security.Cryptography;
+﻿using System.Security.Cryptography;
 
 namespace Transport.Upload._3DModelsUpload.CGTrader._3DModelComponents;
 
-internal record CGTrader3DModelPreviewImage
+internal record CGTrader3DModelPreviewImage : _3DModelPreviewImage
 {
-    internal readonly FileStream FileStream;
-    internal string FileName => Path.GetFileName(FileStream.Name);
-    internal readonly string ModelDraftID;
-    internal ContentType MimeType => new(MimeTypes.GetMimeType(FileStream.Name));
     internal async ValueTask<string> ChecksumAsync(CancellationToken cancellationToken)
     {
         if (_checksum is null)
         {
-            using var hasher = new HMACMD5();
+            using var hasher = MD5.Create();
+            using var fileStream = this.AsFileStream;
             _checksum = Convert.ToBase64String(
-                await hasher.ComputeHashAsync(FileStream, cancellationToken)
+                await hasher.ComputeHashAsync(fileStream, cancellationToken)
                 );
         }
         return _checksum;
     }
     string? _checksum;
 
-    internal string? FileID
+    internal CGTrader3DModelPreviewImage(string path) : base(path)
     {
-        get => _fileId;
-        set
-        {
-            if (_fileId is not null) throw new InvalidOperationException(
-                $"{nameof(FileID)} is lateinit"
-                );
-            else _fileId = value;
-        }
-    }
-    string? _fileId;
-
-    internal string? SignedFileID
-    {
-        get => _signedFileId;
-        set
-        {
-            if (_signedFileId is not null) throw new InvalidOperationException(
-                $"{nameof(SignedFileID)} is lateinit"
-                );
-            else _signedFileId = value;
-        }
-    }
-    string? _signedFileId;
-
-    internal string? LocationOnServer
-    {
-        get => _locationOnServer;
-        set
-        {
-            if (_locationOnServer is not null) throw new InvalidOperationException(
-                $"{nameof(LocationOnServer)} is lateinit"
-                );
-            else _locationOnServer = value;
-        }
-    }
-    string? _locationOnServer;
-
-    internal CGTrader3DModelPreviewImage(
-        FileStream fileStream,
-        string modelDraftId)
-    {
-        FileStream = fileStream;
-        ModelDraftID = modelDraftId;
     }
 }
