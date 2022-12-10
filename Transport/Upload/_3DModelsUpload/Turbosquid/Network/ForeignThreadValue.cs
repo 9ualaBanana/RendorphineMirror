cfa4739bc98f1d068a443bc25fe669919989178a
GIBSON <9ualabanana@gmail.com>
2022-12-10 12:19:39 +0400

    Add `ForeignThreadValue` class.

---
+++ Transport/Upload/_3DModelsUpload/Turbosquid/Network/ForeignThreadValue.cs
@@ -0,0 +1,22 @@
+﻿namespace Transport.Upload._3DModelsUpload.Turbosquid.Network;
+
+public class ForeignThreadValue<T>
+{
+    readonly bool _autoReset;
+
+    public ForeignThreadValue(bool autoReset) => _autoReset = autoReset;
+
+    public async Task<T> GetAsync(CancellationToken cancellationToken = default) => await Task.Run(() =>
+    {
+        _waiter.Wait();
+        if (_autoReset) _waiter.Reset();
+        return _value!;
+    }, cancellationToken);
+    public async Task SetAsync(T value, CancellationToken cancellationToken = default) => await Task.Run(() => 
+    {
+        _value = value;
+        _waiter.Set(); 
+    }, cancellationToken);
+    T? _value;
+    readonly ManualResetEventSlim _waiter = new(false);
+}
