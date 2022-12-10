4140ebf74dc11971fc6c2ae392cfe2b1fae9ffd3
GIBSON <9ualabanana@gmail.com>
2022-12-10 12:22:14 +0400

    Add `TurboSquidApplicationUserID` class.

---
+++ Transport/Upload/_3DModelsUpload/Turbosquid/Network/Authenticity/TurboSquidApplicationUserID.cs
@@ -0,0 +1,12 @@
+﻿namespace Transport.Upload._3DModelsUpload.Turbosquid.Network.Authenticity;
+
+internal static class TurboSquidApplicationUserID
+{
+    internal static string _Parse(string html)
+    {
+        const string userApplicationUidKey = "id=\"user_application_uid\" value=\"";
+        int userApplicationUidStartIndex = html.IndexOf(userApplicationUidKey) + userApplicationUidKey.Length;
+        int userApplicationUidEndIndex = html.IndexOf('"', userApplicationUidStartIndex);
+        return html[userApplicationUidStartIndex..userApplicationUidEndIndex];
+    }
+}
