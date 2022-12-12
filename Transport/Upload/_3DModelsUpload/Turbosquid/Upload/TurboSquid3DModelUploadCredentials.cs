ad127bf6ea3e8feca12041b1c1681e5787ce0b19
GIBSON <9ualabanana@gmail.com>
2022-12-13 00:43:24 +0400

    Create TurboSquid3DModelUploadCredentials.cs

---
+++ Transport/Upload/_3DModelsUpload/Turbosquid/Upload/TurboSquid3DModelUploadCredentials.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Transport.Upload._3DModelsUpload.Turbosquid.Upload;
+
+internal record TurboSquid3DModelUploadCredentials
+{
+    internal readonly string AccessKey;
+    internal readonly string SessionToken;
+    internal readonly string KeyPrefix;
+    internal readonly string Bucket;
+    internal readonly string Region;
+    internal readonly string CurrentServerTime;
+
+    internal static async Task<TurboSquid3DModelUploadCredentials> _AsyncFrom(HttpResponseMessage response)
+    {
+        var uploadCredentialsJson = JObject.Parse(await response.Content.ReadAsStringAsync());
+        return new(
+            (string)uploadCredentialsJson["access_key"]!,
+            (string)uploadCredentialsJson["session_token"]!,
+            (string)uploadCredentialsJson["key_prefix"]!,
+            (string)uploadCredentialsJson["bucket"]!,
+            (string)uploadCredentialsJson["region"]!,
+            // Consider using DateTimeOffset.
+            (string)uploadCredentialsJson["current_server_time"]!);
+    }
+
+    TurboSquid3DModelUploadCredentials(
+        string accessKey,
+        string sessionToken,
+        string keyPrefix,
+        string bucket,
+        string region,
+        string currentServerTime)
+    {
+        AccessKey = accessKey;
+        SessionToken = sessionToken;
+        KeyPrefix = keyPrefix;
+        Bucket = bucket;
+        Region = region;
+        CurrentServerTime = currentServerTime;
+    }
+}
