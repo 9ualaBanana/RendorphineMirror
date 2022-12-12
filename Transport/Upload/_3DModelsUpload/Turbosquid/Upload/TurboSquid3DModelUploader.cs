54a067e67b5e5da8c1c672212ec13af75bda4b96
GIBSON <9ualabanana@gmail.com>
2022-12-13 00:45:30 +0400

    Create TurboSquid3DModelUploader.cs

---
+++ Transport/Upload/_3DModelsUpload/Turbosquid/Upload/TurboSquid3DModelUploader.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using Transport.Upload._3DModelsUpload.Turbosquid.Api;
+using Transport.Upload._3DModelsUpload.Turbosquid.Network.Authenticity;
+
+namespace Transport.Upload._3DModelsUpload.Turbosquid.Upload;
+
+internal class TurboSquid3DModelUploader : I3DModelUploader
+{
+    readonly TurboSquidApi _api;
+
+    internal TurboSquid3DModelUploader()
+    {
+        _api = new();
+    }
+
+    public async Task UploadAsync(
+        Composite3DModel composite3DModel,
+        NetworkCredential credential,
+        CancellationToken cancellationToken)
+    {
+        var credential_ = await TurboSquidNetworkCredential._RequestAsyncUsing(_api, credential, cancellationToken);
+        await _api._LoginAsync(credential_, cancellationToken);
+        var uploadSessionData = await _api._RequestModelUploadSessionDataAsyncFor(composite3DModel, cancellationToken);
+    }
+}
