c5f70cab624569aae7dcde4f711b8f0ccdd981e3
i3ym <deniso99@mail.ru>
2022-12-13 18:25:13 +0700

    Remove CefNet

--- NodeUI/Pages/WebWindow.cs
+++
@@ -1,8 +0,0 @@
-namespace NodeUI.Pages;
-
-public class WebWindow : Window
-{
-    public CefNet.Avalonia.WebView View => ((WebView) Content).View;
-
-    public WebWindow() => Content = new WebView();
-}
