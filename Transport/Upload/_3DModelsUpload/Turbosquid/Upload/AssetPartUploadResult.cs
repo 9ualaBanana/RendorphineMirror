3c0ed3fe530ad822971110e3edfe354e0a5de411
GIBSON <9ualabanana@gmail.com>
2022-12-19 09:06:27 +0400

    Add abstractions for multipart asset upload.

---
+++ Transport/Upload/_3DModelsUpload/Turbosquid/Upload/AssetPartUploadResult.cs
@@ -0,0 +1,12 @@
+﻿using System.Xml.Linq;
+
+namespace Transport.Upload._3DModelsUpload.Turbosquid.Upload;
+
+internal record struct AssetPartUploadResult(int PartNumber, string ETag)
+{
+    internal XElement _ToXElementIn(XNamespace ns) =>
+        new(ns + "Part",
+            new XElement(ns + "ETag", ETag),
+            new XElement(ns + "PartNumber", PartNumber)
+            );
+}
