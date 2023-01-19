1a88bcb5153719c28f7df0c2b968ee91a9673d1d
GIBSON <9ualabanana@gmail.com>
2023-01-19 14:14:01 +0400

    Add `UpdateContext` and corresponding middleware.

---
+++ Telegram/Middleware/UpdateRouting/UpdateContextConstructorMiddleware.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System.Diagnostics.CodeAnalysis;
+using Telegram.Bot.Types;
+using Telegram.Models;
+
+namespace Telegram.Middleware.UpdateRouting;
+
+public class UpdateContextConstructorMiddleware : IMiddleware
+{
+    readonly ILogger _logger;
+
+    public UpdateContextConstructorMiddleware(ILogger<UpdateContextConstructorMiddleware> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        var update = await DeserializeUpdateAsyncFrom(context);
+        EnsureUpdateIsSuccessfullyDeserialized(update);
+
+        new UpdateContext(update).CacheInside(context);
+        _logger.LogTrace($"{nameof(UpdateContext)} is constructed");
+
+        await next(context);
+    }
+
+    static async Task<Update?> DeserializeUpdateAsyncFrom(HttpContext context)
+    {
+        // TODO: Remove after refactoring. Added temporary to allow non-refactored action method perform model binding.
+        context.Request.EnableBuffering();
+        // We have to create a buffer stream because request body must be read asynchronously which is not supported by Newtonsoft.Json (like WTF fr?)
+        // Task.Run() doesn't help in this case as it doesn't make deserialization truly async.
+        using var bufferStream = new MemoryStream();
+        await context.Request.Body.CopyToAsync(bufferStream, context.RequestAborted);
+        bufferStream.Position = 0;
+        // TODO: Remove after refactoring. Added temporary to allow non-refactored action method perform model binding.
+        context.Request.Body.Position = 0;
+        using var streamReader = new StreamReader(bufferStream);
+        using var jsonReader = new JsonTextReader(streamReader);
+        return await Task.Run(() => JsonSerializer.CreateDefault().Deserialize<Update>(jsonReader));
+    }
+
+    void EnsureUpdateIsSuccessfullyDeserialized([NotNull] Update? update)
+    {
+        if (update is null)
+        {
+            string errorMessage = $"Request body didn't contain JSON representing {nameof(Update)}";
+            _logger.LogCritical(errorMessage);
+            throw new InvalidDataException(errorMessage);
+        }
+    }
+}
---
+++ Telegram/Models/UpdateContext.cs
@@ -0,0 +1,35 @@
+﻿using Telegram.Bot.Types;
+using Telegram.Middleware.UpdateRouting;
+
+namespace Telegram.Models;
+
+// Authentication/Authorization info should be stored here as well and be populated by corresponding middleware.
+// It likely should represent M+ user.
+internal class UpdateContext
+{
+    internal readonly Update Update;
+
+    internal UpdateContext(Update update)
+    {
+        Update = update;
+    }
+
+    // Define a separate static class for that functionality.
+
+    /// <summary>
+    /// Adds this instance to <see cref="HttpContext.Items"/> which can be retrieved using <see cref="RetrieveFromCacheOf(HttpContext)"/>.
+    /// </summary>
+    /// <remarks>
+    /// Intended to be called once per request by <see cref="UpdateContextConstructorMiddleware"/>.
+    /// Following call attempts will throw.
+    /// </remarks>
+    /// <exception cref="ArgumentException">Thrown when this method is called more than once per request.</exception>
+    internal void CacheInside(HttpContext context) => context.Items.Add(ItemsKey, this);
+
+    /// <summary>
+    /// Retrieves <see cref="UpdateContext"/> instance stored inside <see cref="HttpContext.Items"/>
+    /// </summary>
+    internal static UpdateContext RetrieveFromCacheOf(HttpContext httpContext) => (httpContext.Items[ItemsKey] as UpdateContext)!;
+
+    static readonly object ItemsKey = new();
+}
