f856bc95975512370ba22aae1a7dbae587852998
i3ym <deniso99@mail.ru>
2023-02-07 16:10:42 +0700

    Add logging for task semaphores

--- Node/PortForwarder.cs
+++ Node/PortForwarder.cs
@@ -45,8 +45,10 @@ namespace Node
                     var mapping = mappings.FirstOrDefault(x => x.Description == name);
                     if (mapping is not null)
                     {
-                        _logger.Info("[UPnP] Found already existing mapping: {Mapping}", mapping);
-                        portb.Value = (ushort) mapping.PublicPort;
+                        _logger.Info($"[UPnP] Found already existing mapping {mapping}. Remapping...");
+
+                        device.DeletePortMap(mapping);
+                        device.CreatePortMap(new Mapping(Protocol.Tcp, portb.Value, portb.Value, 0, name));
                     }
                     else
                     {
