18bfc461560940fbc81bcdeb9905cd7c9cf03c8d
GIBSON <9ualabanana@gmail.com>
2023-02-11 10:13:12 +0400

    Send optional `autoremovetimer` when registering tasks.

--- NodeCommon/Tasks/TaskRegistration.cs
+++ NodeCommon/Tasks/TaskRegistration.cs
@@ -48,6 +48,8 @@ public static class TaskRegistration
             var soft = new[] { new TaskSoftwareRequirement(info.Type.ToString().ToLowerInvariant(), ImmutableArray.Create(info.Version), null), };
             values.Add(("software", JsonConvert.SerializeObject(soft, JsonSettings.LowercaseIgnoreNull)));
         }
+        if (output is MPlusTaskOutputInfo mPlusOutput && mPlusOutput.AutoremoveTimer is not null)
+            values.Add(("autoremovetimer", mPlusOutput.AutoremoveTimer.ToString()!));
 
         var logtext = $"Registering task: {string.Join("; ", values.Skip(1).Select(x => x.Item1 + ": " + x.Item2))}";
         if (log is not null) log.LogInfo(logtext);
