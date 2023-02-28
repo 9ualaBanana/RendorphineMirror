2c29782d21cbe471878cf18b0507f7640211c0d5
GIBSON <9ualabanana@gmail.com>
2023-02-28 18:33:10 +0400

    Define data structures for persistence of the bot users.

---
+++ Telegram/Security/Authentication/Persistence/MPlusIdentityEntity.cs
@@ -0,0 +1,19 @@
+﻿namespace Telegram.Security.Authentication.Persistence;
+
+/// <summary>
+/// Entity representing <see cref="MPlusIdentity"/> of the user represented by <see cref="TelegramBotUserEntity"/>.
+/// </summary>
+public record MPlusIdentityEntity : MPlusIdentity
+{
+    public TelegramBotUserEntity TelegramBotUser { get; set; } = null!;
+
+    internal MPlusIdentityEntity(MPlusIdentity mPlusIdentity)
+        : base(mPlusIdentity)
+    { 
+    }
+
+    internal MPlusIdentityEntity(string userId, string sessionId, AccessLevel accessLevel)
+        : base(userId, sessionId, accessLevel)
+    {
+    }
+}
---
+++ Telegram/Security/Authentication/Persistence/TelegramBotUserEntity.cs
@@ -0,0 +1,11 @@
+﻿using Telegram.Bot.Types;
+
+namespace Telegram.Security.Authentication.Persistence;
+
+/// <summary>
+/// Principal entity with which other entities representing information related to this <see cref="ChatId"/> are associated.
+/// </summary>
+public record TelegramBotUserEntity(ChatId ChatId)
+{
+    public MPlusIdentityEntity? MPlusIdentity { get; set; }
+}
