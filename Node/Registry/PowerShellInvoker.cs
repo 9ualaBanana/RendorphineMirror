a9c2f413eeccbad560cf340e8eea312eebd7fe17
i3ym <deniso99@mail.ru>
2023-03-22 19:53:07 +0700

    Add Download-Run-Installer cmdlet

--- Node/Registry/PowerShellInvoker.cs
+++ Node/Registry/PowerShellInvoker.cs
@@ -5,34 +5,43 @@ namespace Node.Registry;
 
 public static class PowerShellInvoker
 {
-    readonly static Logger _logger = LogManager.GetCurrentClassLogger();
+    public static readonly Logger Logger = LogManager.GetLogger("PLUGINS");
 
     public static IReadOnlyCollection<PSObject> InstallPlugin(PluginToDeploy plugin, string script, bool deleteInstaller)
     {
-        string pldownload = null!;
-        var invok = Invoke(script, setVars);
+        var pwsh = Initialize(script);
+        var pldownload = setVars(pwsh);
+        var result = Invoke(pwsh);
 
         if (deleteInstaller && Directory.Exists(pldownload))
             Directory.Delete(pldownload, true);
 
-        return invok;
+        return result;
 
 
-        void setVars(PowerShell psh)
+        /// <returns> Plugin download directory </returns>
+        string setVars(PowerShell psh)
         {
             var prox = psh.Runspace.SessionStateProxy;
             var plugintype = plugin.Type.ToString().ToLowerInvariant();
             var pluginver = plugin.Version;
             Path.GetInvalidFileNameChars().ToList().ForEach(x => pluginver = pluginver.Replace(x, '_'));
 
+            prox.SetVariable("PLUGIN", plugintype);
+            prox.SetVariable("PLUGINVER", pluginver);
+
             prox.SetVariable("PLTORRENT", plugintype + "." + pluginver);
 
-            prox.SetVariable("PLDOWNLOAD", pldownload = Path.Combine(prox.GetVariable("DOWNLOADS").ToString()!, plugintype, pluginver));
+            var pldownload = Path.Combine(prox.GetVariable("DOWNLOADS").ToString()!, plugintype, pluginver);
+            prox.SetVariable("PLDOWNLOAD", pldownload);
             prox.SetVariable("PLINSTALL", Path.Combine(prox.GetVariable("PLUGINS").ToString()!, plugintype, pluginver));
+
+            return pldownload;
         }
     }
 
-    public static IReadOnlyCollection<PSObject> Invoke(string script, Action<PowerShell>? runspaceModifFunc = null)
+
+    public static PowerShell Initialize(string script)
     {
         var runspace = RunspaceFactory.CreateRunspace();
         // TODO: restrictions on commands?
@@ -42,7 +51,6 @@ public static class PowerShellInvoker
         AddVariables(runspace);
 
         var psh = PowerShell.Create(runspace);
-        runspaceModifFunc?.Invoke(psh);
 
         psh.AddStatement()
             .AddCommand("Import-Module")
@@ -51,6 +59,10 @@ public static class PowerShellInvoker
         psh.AddStatement()
             .AddScript(script);
 
+        return psh;
+    }
+    public static IReadOnlyCollection<PSObject> Invoke(PowerShell psh)
+    {
         var result = psh.Invoke(Array.Empty<object>(), new PSInvocationSettings() { ErrorActionPreference = ActionPreference.Stop });
         if (psh.InvocationStateInfo.Reason is not null)
             throw psh.InvocationStateInfo.Reason;
