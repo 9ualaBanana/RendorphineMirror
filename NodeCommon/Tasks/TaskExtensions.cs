91c0e9fdf1fadbcbd0eaf4f9e2a4fa0386dd2c8b
i3ym <deniso99@mail.ru>
2023-03-22 20:39:43 +0700

    Add LoggableLogger for logging user logger

--- NodeCommon/Tasks/TaskExtensions.cs
+++ NodeCommon/Tasks/TaskExtensions.cs
@@ -4,10 +4,12 @@ public static class TaskExtensions
 {
     readonly static Logger _logger = LogManager.GetCurrentClassLogger();
 
-    public static void Log(this ILoggable obj, LogLevel level, string text) => _logger.Log(level, $"[{obj.LogName}] {text}");
-    public static void LogInfo(this ILoggable obj, string text) => _logger.Info($"[{obj.LogName}] {text}");
-    public static void LogWarn(this ILoggable obj, string text) => _logger.Warn($"[{obj.LogName}] {text}");
-    public static void LogErr(this ILoggable obj, string text) => _logger.Error($"[{obj.LogName}] {text}");
+    public static void Log(this ILoggable obj, LogLevel level, string text) =>
+        ((obj as LoggableLogger)?.Logger ?? _logger).Log(level, $"[{obj.LogName}] {text}");
+
+    public static void LogInfo(this ILoggable obj, string text) => _logger.Log(LogLevel.Info, $"[{obj.LogName}] {text}");
+    public static void LogWarn(this ILoggable obj, string text) => _logger.Log(LogLevel.Warn, $"[{obj.LogName}] {text}");
+    public static void LogErr(this ILoggable obj, string text) => _logger.Log(LogLevel.Error, $"[{obj.LogName}] {text}");
     public static void LogErr(this ILoggable obj, Exception ex) => obj.LogErr(ex.ToString());
-    public static void LogTrace(this ILoggable obj, string text) => _logger.Trace($"[{obj.LogName}] {text}");
+    public static void LogTrace(this ILoggable obj, string text) => _logger.Log(LogLevel.Trace, $"[{obj.LogName}] {text}");
 }
