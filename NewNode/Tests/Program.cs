8315aafda5c32bb260af0b8534ce1099cc6c2759
i3ym <deniso99@mail.ru>
2023-05-11 14:38:59 +0700

    Add project for testing

---
+++ NewNode/Tests/Program.cs
@@ -0,0 +1,49 @@
+﻿using Node.Plugins;
+using Node.Plugins.Discoverers;
+using Node.Plugins.Models;
+using Node.Tasks;
+using Node.Tasks.Exec.Actions;
+using Node.Tasks.Models;
+
+Initializer.AppName = "renderfin";
+
+PluginsManager.RegisterPluginDiscoverers(
+    new BlenderPluginDiscoverer(),
+    new Autodesk3dsMaxPluginDiscoverer(),
+    new TopazGigapixelAIPluginDiscoverer(),
+    new DaVinciResolvePluginDiscoverer(),
+    new FFmpegPluginDiscoverer(),
+    new PythonPluginDiscoverer(),
+    new PythonEsrganPluginDiscoverer(),
+    new RobustVideoMattingPluginDiscoverer(),
+    new VeeeVectorizerPluginDiscoverer(),
+    new NvidiaDriverPluginDiscoverer(),
+    new CondaPluginDiscoverer()
+);
+await PluginsManager.DiscoverInstalledPluginsAsync();
+
+
+
+await new EsrganUpscale().Execute(new TestContext(),
+    new TaskFiles(
+        new ReadOnlyTaskFileList(new[]
+        {
+            new FileWithFormat(FileFormat.Jpeg, "/home/i3ym/Загрузки/Telegram Desktop/2022-05-29 13-06-22.JPG")
+        }),
+        new TaskFileListList("/temp/asd")
+    ),
+    new UpscaleEsrganInfo()
+);
+
+
+
+class TestContext : ITaskExecutionContext
+{
+    public IReadOnlyCollection<Plugin> Plugins => PluginsManager.GetInstalledPluginsCache().ThrowIfNull();
+
+    public void Log(LogLevel level, string text) =>
+        Console.WriteLine($"[{level} : Testing] {text}");
+
+    public void SetProgress(double progress) =>
+        Log(LogLevel.Info, $"Task progress: {(int) (progress * 100)}%");
+}
