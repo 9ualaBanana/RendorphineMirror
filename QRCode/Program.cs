f264782593ace0bde405b6277eed09acaa18f4e3
GIBSON <9ualabanana@gmail.com>
2023-07-15 14:59:36 +0400

    Implement bare minimum QR code generation.

---
+++ QRCode/Program.cs
@@ -0,0 +1,11 @@
+﻿using IronBarCode;
+
+var resourceArg = args.FirstOrDefault();
+ArgumentNullException.ThrowIfNull(resourceArg, nameof(resourceArg));
+var resource = new Uri(resourceArg, UriKind.Absolute);
+
+var logoArg = args.ElementAtOrDefault(1);
+ArgumentNullException.ThrowIfNull(logoArg, nameof(logoArg));
+var logo = new Uri(logoArg, UriKind.Absolute);
+
+var qrCode = QRCodeWriter.CreateQrCodeWithLogo(resource.AbsoluteUri, new QRCodeLogo(logo));
