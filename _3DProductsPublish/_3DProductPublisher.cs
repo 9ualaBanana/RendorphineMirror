32a1f895ae4110210cb1bde9da9e953807c4d5c0
GIBSON <9ualabanana@gmail.com>
2023-09-10 13:55:31 +0400

    Improve `_3DProductPublisher` interface.

--- _3DProductsPublish/_3DProductPublisher.cs
+++ _3DProductsPublish/_3DProductPublisher.cs
@@ -1,31 +1,34 @@
 ﻿using _3DProductsPublish._3DProductDS;
-using _3DProductsPublish.Turbosquid._3DModelComponents;
+using _3DProductsPublish.CGTrader.Upload;
 using _3DProductsPublish.Turbosquid.Upload;
 using System.Net;
 
 namespace _3DProductsPublish;
 
-public static class _3DProductPublisher
+public class _3DProductPublisher
 {
-    public static async Task PublishAsync(
-        this _3DProduct _3DProduct,
+    readonly CGTrader3DProductPublisher _cgTrader;
+    readonly TurboSquid3DProductPublisher _turboSquid;
+
+    public static async Task<_3DProductPublisher> InitializeAsync(NetworkCredential credential, CancellationToken cancellationToken = default)
+        => new(
+            new CGTrader3DProductPublisher(),
+            await TurboSquid3DProductPublisher.InitializeAsync(credential, cancellationToken)
+            );
+
+    _3DProductPublisher(CGTrader3DProductPublisher cgTrader, TurboSquid3DProductPublisher turboSquid)
+    {
+        _cgTrader = cgTrader;
+        _turboSquid = turboSquid;
+    }
+
+    public async Task PublishAsync(
+        _3DProduct _3DProduct,
         _3DProduct.Metadata_ metadata,
         NetworkCredential credential,
         CancellationToken cancellationToken = default)
     {
-        await
-            (await TurboSquid3DProductPublisher.InitializeAsync(credential, cancellationToken))
-            .PublishAsync(await _3DProduct.AsyncWithTurboSquid(metadata, cancellationToken), credential, cancellationToken);
-        // It should be published to each stock and not switched on metadata as it should be generic to be used for each stock and obtain some specific metadata in further processing steps.
-        //await (_3DProduct.Metadata switch
-        //{
-        //    CGTrader3DProductMetadata =>
-        //        new CGTrader3DProductPublisher().PublishAsync(_3DProduct, credential, cancellationToken),
-        //    TurboSquid3DProductMetadata =>
-        //        new TurboSquid3DProductPublisher().PublishAsync(_3DProduct, credential, cancellationToken),
-        //    { } unsupportedType => throw new ArgumentOutOfRangeException(
-        //        nameof(unsupportedType), unsupportedType.GetType(), "Unsupported metadata type."
-        //        )
-        //});
+        await _cgTrader.PublishAsync(_3DProduct.WithCGTrader(metadata), credential, cancellationToken);
+        await _turboSquid.PublishAsync(await _3DProduct.AsyncWithTurboSquid(metadata, cancellationToken), credential, cancellationToken);
     }
 }
