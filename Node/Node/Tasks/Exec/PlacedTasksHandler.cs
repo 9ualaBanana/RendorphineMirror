98f3b81e1e5c85d98871d8ec1fca4069c6f35d1b
i3ym <deniso99@mail.ru>
2023-09-13 19:25:07 +0700

    Move out local methods in PlacedTasksHandler

--- Node/Node/Tasks/Exec/PlacedTasksHandler.cs
+++ Node/Node/Tasks/Exec/PlacedTasksHandler.cs
@@ -10,6 +10,8 @@ public class PlacedTasksHandler
     public required IIndex<TaskOutputType, ITaskCompletionHandler> CompletionHandlers { get; init; }
     public required ILogger<PlacedTasksHandler> Logger { get; init; }
 
+    string? ScGuid;
+
     public async Task InitializePlacedTasksAsync() =>
         await Task.WhenAll(PlacedTasks.PlacedTasks.Values.ToArray().Select(UploadInputFiles));
 
@@ -55,149 +57,146 @@ public class PlacedTasksHandler
     /// <summary> Polls all non-finished placed tasks, sets their state to Finished, Canceled, Failed if needed </summary>
     public void StartUpdatingPlacedTasks()
     {
-        var scguid = null as string;
-
         new Thread(async () =>
         {
             while (true)
             {
-                try { await checkAll(); }
+                try { await CheckAll(); }
                 catch (Exception ex) { Logger.Error(ex); }
 
                 await Task.Delay(30_000);
             }
         })
         { IsBackground = true }.Start();
+    }
 
+    async ValueTask CheckAll()
+    {
+        if (PlacedTasks.PlacedTasks.Count == 0) return;
 
-        async ValueTask checkAll()
+        var copy = PlacedTasks.PlacedTasks.Values.ToArray();
+        var emptysharded = copy.Where(x => x.HostShard is null).ToArray();
+        await Api.UpdateTaskShardsAsync(emptysharded).ThrowIfError();
+
+        foreach (var es in emptysharded)
+            PlacedTasks.PlacedTasks.Save(es);
+
+        copy = copy.Where(x => x.HostShard is not null).ToArray();
+
+        var checkedtasks = new List<string>();
+        foreach (var group in copy.GroupBy(x => x.HostShard))
         {
-            if (PlacedTasks.PlacedTasks.Count == 0) return;
-
-            var copy = PlacedTasks.PlacedTasks.Values.ToArray();
-            var emptysharded = copy.Where(x => x.HostShard is null).ToArray();
-            await Api.UpdateTaskShardsAsync(emptysharded).ThrowIfError();
-
-            foreach (var es in emptysharded)
-                PlacedTasks.PlacedTasks.Save(es);
-
-            copy = copy.Where(x => x.HostShard is not null).ToArray();
-
-            var checkedtasks = new List<string>();
-            foreach (var group in copy.GroupBy(x => x.HostShard))
+            var act = await Api.GetTasksOnShardAsync(group.Key!).ThrowIfError();
+            if (ScGuid is null) ScGuid = act.ScGuid;
+            else if (ScGuid != act.ScGuid)
             {
-                var act = await Api.GetTasksOnShardAsync(group.Key!).ThrowIfError();
-                if (scguid is null) scguid = act.ScGuid;
-                else if (scguid != act.ScGuid)
-                {
-                    scguid = act.ScGuid;
-                    Logger.Info($"!! Task configuration changed ({act.ScGuid}), resetting all task shards...");
-                    foreach (var task in PlacedTasks.PlacedTasks)
-                        task.Value.HostShard = null;
+                ScGuid = act.ScGuid;
+                Logger.Info($"!! Task configuration changed ({act.ScGuid}), resetting all task shards...");
+                foreach (var task in PlacedTasks.PlacedTasks)
+                    task.Value.HostShard = null;
 
-                    return;
-                }
-
-                await process(TaskState.Input, act.Input);
-                await process(TaskState.Active, act.Active);
-                await process(TaskState.Output, act.Output);
-                await process(TaskState.Validation, act.Validation);
-
-                async ValueTask process(TaskState state, ImmutableArray<TMTaskStateInfo> tasks)
-                {
-                    var changed = tasks.Where(x => PlacedTasks.PlacedTasks.TryGetValue(x.Id, out var task) && task.State != state).Select(x => x.Id).ToArray();
-                    if (changed.Length != 0)
-                        Logger.Info($"Tasks changed to {state}: {string.Join(", ", changed)}");
-
-                    foreach (var task in tasks)
-                        await processTask(task.Id, task, state);
-                    checkedtasks.AddRange(tasks.Select(x => x.Id));
-                }
-            }
-
-            var finished = await Api.GetFinishedTasksStatesAsync(copy.Select(x => x.Id).Except(checkedtasks)).ThrowIfError();
-            foreach (var (id, state) in finished)
-                await processTask(id, state, null);
-
-
-            async ValueTask processTask(string taskid, ITaskStateInfo state, TaskState? newstate)
-            {
-                if (!PlacedTasks.PlacedTasks.TryGetValue(taskid, out var task)) return;
-
-                using var _logscope = Logger.BeginScope($"PTask {task.Id}");
-                task.State = newstate ?? task.State;
-                task.Populate(state);
-                if (task.State == TaskState.Validation)
-                    task.Populate(await Api.GetTaskStateAsyncOrThrow(task).ThrowIfError());
-
-                try { await check(task, (state as TMOldTaskStateInfo)?.ErrMsg); }
-                catch (TaskFailedException ex)
-                {
-                    await Api.ChangeStateAsync(task, TaskState.Canceled).ThrowIfError();
-                    remove(task, ex.Message);
-                }
-                catch (Exception ex) { Logger.LogError(ex, ""); }
-            }
-        }
-        async ValueTask check(DbTaskFullState task, string? errmsg)
-        {
-            if (task.State.IsFinished())
-            {
-                remove(task, errmsg);
                 return;
             }
 
-            if (task.State < TaskState.Output) return;
-            if (task.State is TaskState.Canceled or TaskState.Failed)
+            await process(TaskState.Input, act.Input);
+            await process(TaskState.Active, act.Active);
+            await process(TaskState.Output, act.Output);
+            await process(TaskState.Validation, act.Validation);
+
+            async ValueTask process(TaskState state, ImmutableArray<TMTaskStateInfo> tasks)
             {
-                remove(task, errmsg);
-                return;
+                var changed = tasks.Where(x => PlacedTasks.PlacedTasks.TryGetValue(x.Id, out var task) && task.State != state).Select(x => x.Id).ToArray();
+                if (changed.Length != 0)
+                    Logger.Info($"Tasks changed to {state}: {string.Join(", ", changed)}");
+
+                foreach (var task in tasks)
+                    await processTask(task.Id, task, state);
+                checkedtasks.AddRange(tasks.Select(x => x.Id));
             }
-
-            var completed =
-                CompletionCheckers.TryGetValue(task.Output.Type, out var checker)
-                ? checker.CheckCompletion(task.Output, task.State)
-                : task.State == TaskState.Validation;
-            if (!completed) return;
-
-            if (CompletionHandlers.TryGetValue(task.Output.Type, out var handler))
-                await handler.OnPlacedTaskCompleted(task.Output);
-
-            await Api.ChangeStateAsync(task, TaskState.Finished).ThrowIfError();
-            remove(task, errmsg);
         }
-        bool remove(DbTaskFullState task, string? errmsg)
+
+        var finished = await Api.GetFinishedTasksStatesAsync(copy.Select(x => x.Id).Except(checkedtasks)).ThrowIfError();
+        foreach (var (id, state) in finished)
+            await processTask(id, state, null);
+
+
+        async ValueTask processTask(string taskid, ITaskStateInfo state, TaskState? newstate)
         {
-            Logger.LogInformation($"{task.State}, removing" + (errmsg is null ? null : $" ({errmsg})"));
-            PlacedTasks.PlacedTasks.Remove(task);
+            if (!PlacedTasks.PlacedTasks.TryGetValue(taskid, out var task)) return;
 
-            foreach (var wtask in WatchingTasks.WatchingTasks.Values.ToArray())
+            using var _logscope = Logger.BeginScope($"PTask {task.Id}");
+            task.State = newstate ?? task.State;
+            task.Populate(state);
+            if (task.State == TaskState.Validation)
+                task.Populate(await Api.GetTaskStateAsyncOrThrow(task).ThrowIfError());
+
+            try { await Check(task, (state as TMOldTaskStateInfo)?.ErrMsg); }
+            catch (TaskFailedException ex)
             {
-                if (!wtask.PlacedNonCompletedTasks.Contains(task.Id)) continue;
-
-                wtask.Complete(task);
-                wtask.PlacedNonCompletedTasks.Remove(task.Id);
-                WatchingTasks.WatchingTasks.Save(wtask);
+                await Api.ChangeStateAsync(task, TaskState.Canceled).ThrowIfError();
+                Remove(task, ex.Message);
             }
+            catch (Exception ex) { Logger.LogError(ex, ""); }
+        }
+    }
+    async ValueTask Check(DbTaskFullState task, string? errmsg)
+    {
+        if (task.State.IsFinished())
+        {
+            Remove(task, errmsg);
+            return;
+        }
 
-            if (errmsg?.Contains("There is no such user.", StringComparison.Ordinal) == true)
+        if (task.State < TaskState.Output) return;
+        if (task.State is TaskState.Canceled or TaskState.Failed)
+        {
+            Remove(task, errmsg);
+            return;
+        }
+
+        var completed =
+            CompletionCheckers.TryGetValue(task.Output.Type, out var checker)
+            ? checker.CheckCompletion(task.Output, task.State)
+            : task.State == TaskState.Validation;
+        if (!completed) return;
+
+        if (CompletionHandlers.TryGetValue(task.Output.Type, out var handler))
+            await handler.OnPlacedTaskCompleted(task.Output);
+
+        await Api.ChangeStateAsync(task, TaskState.Finished).ThrowIfError();
+        Remove(task, errmsg);
+    }
+    bool Remove(DbTaskFullState task, string? errmsg)
+    {
+        Logger.LogInformation($"{task.State}, removing" + (errmsg is null ? null : $" ({errmsg})"));
+        PlacedTasks.PlacedTasks.Remove(task);
+
+        foreach (var wtask in WatchingTasks.WatchingTasks.Values.ToArray())
+        {
+            if (!wtask.PlacedNonCompletedTasks.Contains(task.Id)) continue;
+
+            wtask.Complete(task);
+            wtask.PlacedNonCompletedTasks.Remove(task.Id);
+            WatchingTasks.WatchingTasks.Save(wtask);
+        }
+
+        if (errmsg?.Contains("There is no such user.", StringComparison.Ordinal) == true)
+        {
+            var tuid = (task.Output as MPlusTaskOutputInfo)?.TUid ?? (task.Output as QSPreviewOutputInfo)?.TUid;
+            if (tuid is not null)
             {
-                var tuid = (task.Output as MPlusTaskOutputInfo)?.TUid ?? (task.Output as QSPreviewOutputInfo)?.TUid;
-                if (tuid is not null)
+                Logger.LogWarning($"Found nonexistent user {tuid}, hiding");
+
+                foreach (var wtask in WatchingTasks.WatchingTasks.Values.ToArray())
                 {
-                    Logger.LogWarning($"Found nonexistent user {tuid}, hiding");
+                    if (wtask.Source is MPlusAllFilesWatchingTaskInputInfo handler)
+                        handler.NonexistentUsers.Add(tuid);
 
-                    foreach (var wtask in WatchingTasks.WatchingTasks.Values.ToArray())
-                    {
-                        if (wtask.Source is MPlusAllFilesWatchingTaskInputInfo handler)
-                            handler.NonexistentUsers.Add(tuid);
-
-                        WatchingTasks.WatchingTasks.Save(wtask);
-                    }
+                    WatchingTasks.WatchingTasks.Save(wtask);
                 }
             }
-
-            return true;
         }
+
+        return true;
     }
 }
