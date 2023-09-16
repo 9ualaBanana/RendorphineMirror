57f2db1eded3e06981a7676567a51b07a163060d
i3ym <deniso99@mail.ru>
2023-09-16 16:59:33 +0700

    Fix publishing

--- Node/Node.UI/Apis.cs
+++ Node/Node.UI/Apis.cs
@@ -7,6 +7,6 @@ public static class Apis
 #if DEBUG
     public static Node.Common.Apis Default => new(Api.Default, NodeGlobalState.Instance.AuthInfo.Value?.SessionId ?? "63fe288368974192c27a5388");
 #else
-    public static Node.Common.Apis Default => new(Api.Default, NodeGlobalState.Instance.SessionId.ThrowIfNull("UI is not connected to node"));
+    public static Node.Common.Apis Default => new(Api.Default, (NodeGlobalState.Instance.AuthInfo.Value?.SessionId).ThrowIfNull("UI is not connected to node"));
 #endif
 }
