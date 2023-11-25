eb2e3b45becf226fe773b99411a45fdef5a40b4e
i3ym <deniso99@mail.ru>
2023-11-25 17:39:57 +0700

    Fix MPlusItem.Iid not saving

--- NodeCommon/Tasks/Model/MPlusItemTaskInputInfo.cs
+++ NodeCommon/Tasks/Model/MPlusItemTaskInputInfo.cs
@@ -5,5 +5,5 @@ public class MPlusItemTaskInputInfo : IMPlusTaskInputInfo
     public TaskInputType Type => TaskInputType.MPlusItem;
     public string Iid { get; }
 
-    public MPlusItemTaskInputInfo(string id) => Iid = id;
+    public MPlusItemTaskInputInfo(string iid) => Iid = iid;
 }
