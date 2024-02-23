1734acfe6cd37d999320516a678eca5db422febf
i3ym <deniso99@mail.ru>
2024-02-23 19:29:43 +0700

    Fix the new task checks

--- Node/Node/Tasks/Watching/Handlers/Input/Generate3DRFProductTaskHandler.cs
+++ Node/Node/Tasks/Watching/Handlers/Input/Generate3DRFProductTaskHandler.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using _3DProductsPublish.Turbosquid;
 
 namespace Node.Tasks.Watching.Handlers.Input;
@@ -53,25 +54,25 @@ public class Generate3DRFProductTaskHandler : WatchingTaskInputHandler<Generate3
     {
         foreach (var productDir in Directory.GetDirectories(Input.InputDirectory))
         {
-            if (File.Exists(Path.Combine(productDir, "rfproducted"))) continue;
+            if (File.Exists(Path.Combine(productDir, ".rfproducted"))) continue;
 
             var rfp = await RFProductFactory.CreateAsync(productDir, Directories.DirCreated(Input.RFProductDirectory, Path.GetFileNameWithoutExtension(productDir)), default, false);
             Logger.Info($"Auto-created rfproduct {rfp.ID} @ {rfp.Idea.Path}");
-            File.Create(Path.Combine(productDir, "rfproducted")).Dispose();
+            File.Create(Path.Combine(productDir, ".rfproducted")).Dispose();
         }
     }
     async Task PublishRFProducts(CancellationToken token)
     {
         foreach (var rfproduct in RFProducts.RFProducts.Values.Where(r => r.Type == nameof(RFProduct._3D) && r.Path.StartsWith(Path.GetFullPath(Input.RFProductDirectory))))
         {
-            var submitJson = JObject.Parse(Directory.GetFiles(rfproduct.Idea.Path).Single(f => f.EndsWith("_Submit.json")));
+            var submitJson = JObject.Parse(await File.ReadAllTextAsync(Directory.GetFiles(rfproduct.Idea.Path).Single(f => f.EndsWith("_Submit.json")), token));
 
             if ((submitJson["toSubmitSquid"]?.ToObject<ToSubmit>() ?? ToSubmit.None) == ToSubmit.Submit)
             {
-                if (File.Exists(Path.Combine(rfproduct, "turbosquid.meta")))
+                if (File.Exists(Path.Combine(rfproduct.Idea.Path, "turbosquid.meta")))
                 {
-                    Logger.Info(File.ReadLines(Path.Combine(rfproduct, "turbosquid.meta")).FirstOrDefault() ?? "<empty turbosquid.meta>");
-                    if (File.ReadLines(Path.Combine(rfproduct, "turbosquid.meta")).FirstOrDefault()?.Contains(@"\[\d+\]") ?? false)
+                    var firstline = File.ReadLines(Path.Combine(rfproduct.Idea.Path, "turbosquid.meta")).FirstOrDefault();
+                    if (firstline is not null && Regex.IsMatch(firstline, @"\[\d+\]"))
                         continue;
                 }
 
