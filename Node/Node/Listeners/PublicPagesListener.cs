82ed7bc01931d1eb396166a8e56cf6a308eb5178
i3ym <deniso99@mail.ru>
2024-04-27 18:45:18 +0700

    Remove old page from publicpages

--- Node/Node/Listeners/PublicPagesListener.cs
+++ Node/Node/Listeners/PublicPagesListener.cs
@@ -95,58 +95,6 @@ namespace Node.Listeners
                 }
             }
 
-
-            string getPageScript(string username, string path)
-            {
-                path = path.Replace("/", string.Empty);
-                if (string.IsNullOrWhiteSpace(path))
-                    path = "index";
-
-                return $$"""
-                <!doctype html>
-                <html lang="en">
-                <head>
-                    <meta charset="UTF-8" />
-                    <meta name="viewport" content="width=device-width, initial-scale=1">
-                    <title>Vite + React + TS</title>
-                    <script>
-                        const loadResource = (commitHash, isStyle = false) => {
-                            if (isStyle) {
-                                const link = document.createElement('link');
-                                link.href = `https://cdn.jsdelivr.net/gh/{{username}}/{{path}}@${commitHash}/dist/assets/index.css`;
-                                link.rel = 'stylesheet';
-                                document.head.appendChild(link);
-                            } else {
-                                const script = document.createElement('script');
-                                script.src = `https://cdn.jsdelivr.net/gh/{{username}}/{{path}}@${commitHash}/dist/assets/index.js`;
-                                script.type = 'module';
-                                document.body.appendChild(script);
-                            }
-                        };
-                        fetch('https://api.github.com/repos/{{username}}/{{path}}/commits/main')
-                            .then(response => response.json())
-                            .then(data => {
-                                const commitHash = data.sha;
-                                loadResource(commitHash); // Загрузка скрипта
-                                loadResource(commitHash, true); // Загрузка стилей
-                            });
-                    </script>
-                </head>
-                <body>
-                    <div id="root"></div>
-                </body>
-                </html>
-                """;
-            }
-
-            if (path.Length == 0 || path.StartsWith("rf/", StringComparison.Ordinal))
-            {
-                using var writer = new StreamWriter(response.OutputStream, leaveOpen: true);
-                writer.Write(getPageScript("rfpages", path));
-
-                return HttpStatusCode.OK;
-            }
-
             static JObject rfProductToJson(RFProduct product)
             {
                 return new JObject()
